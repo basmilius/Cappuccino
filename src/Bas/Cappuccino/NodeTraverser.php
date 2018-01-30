7dcc8c1e28112781461076e378f866023da96233
Bas Milius <basmilius@hotmail.com>
2018-01-30 14:14:05 +0100

    More... lol

--- src/Bas/Cappuccino/NodeTraverser.php
+++ src/Bas/Cappuccino/NodeTraverser.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the Bas\Cappuccino package.
+ *
+ * Copyright (c) 2018 - Bas Milius <bas@mili.us>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Bas\Cappuccino;
